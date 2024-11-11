3f56cacf8b9bff298c4959e0540dbd20ba83f0da
farid45219 <refrigerator.rni.eed7@waltonplc.com>
2024-11-11 09:49:51 +0600

    RPM & RPS added

--- Feedback_Fan_RPM.ino
+++ Feedback_Fan_RPM.ino
@@ -9,19 +9,33 @@
 #include "freq.h"
 
 uint32_t pulse_per_sec = 0;
+uint32_t rps, rpm;
 
 void setup() {
   FreqMes_Init();
   Serial.begin(38400);
+  //Init buzzer gpio
 }
 
 void loop() {
   pulse_per_sec  = FreqMes_Timer_Get_AvgTimerValShadow();
   pulse_per_sec *= FreqMes_Timer_Get_TimerTickUnitUs();
   pulse_per_sec  = 1000000/pulse_per_sec;
+  rps            = pulse_per_sec/2;
+  rpm            = rps*60;
   Serial.print("AvgTime ");
   Serial.println(FreqMes_Timer_Get_AvgTimerValShadow());
   Serial.print("PPS     ");
   Serial.println(pulse_per_sec);
+  Serial.print("RPS     ");
+  Serial.println(rps);
+  Serial.print("RPM     ");
+  Serial.println(rpm);
+  Serial.println();
+
+  if(rpm < 1200){
+    //buzzer alaram
+  }
+
   _delay_ms(100);
 }
